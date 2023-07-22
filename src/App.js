b96ed6a5920bc5a31940fffeaf63107766df9d6c
mdshowkatislam <mdshowkatislam8876643@yahoo.com>
2023-07-22 16:12:08 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Demo from './demo'
+
+function App() {
+  return (
+    <div><h1 className="text-8xl underline ">Hello world!</h1>
+      <br />
+      <Demo/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/demo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function demo() {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default demo;
